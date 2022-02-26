5016c8601425bdc6b292108eece0844055dd4652
Squishiest Grape <48263319+Squishiest-Grape@users.noreply.github.com>
2022-02-26 00:29:18 -0800

    Update searchle.js

--- docs/searchle.js
+++ docs/searchle.js
@@ -258,6 +258,8 @@ async function searchleMain(document) {
         console.log(oldOptions)
         console.log(newOptions)
         for (let [key,val] of Object.entries(oldOptions)) {
+            console.log(key)
+            console.log(newOptions)            
             if (key in newOptions && newOptions[key].value !== null) {
                 if (typeof val.value === 'object' && !Array.isArray(val.value) && val.value !== null) {
                     combineOptions(val.value,newOptions[key].value) 
