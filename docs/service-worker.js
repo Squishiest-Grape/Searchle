b47a82f31cfb5698a8be0352263d1a1e2ebfd7c2
Squishiest Grape <48263319+Squishiest-Grape@users.noreply.github.com>
2022-03-09 23:58:16 -0800

    fix ref

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -3,16 +3,15 @@ const cacheName = 'searchle-cache';
 
 // put the static assets and routes you want to cache here
 const filesToCache = [
-  '/',
-  '/index.html',
-  '/favicon.ico',
-  '/help.txt',
-  '/searchle.js',
-  '/../data/wordlist.json',
+  'index.html',
+  'favicon.ico',
+  'help.txt',
+  'searchle.js',
+  '../data/wordlist.json',
 ]
 
 // the event handler for the activate event
-self.addEventListener('activate', e => self.clients.claim());
+self.addEventListener('activate', e => self.clients.claim())
 
 // the event handler for the install event 
 // typically used to cache assets
@@ -20,14 +19,14 @@ self.addEventListener('install', e => {
   e.waitUntil(
     caches.open(cacheName)
     .then(cache => cache.addAll(filesToCache))
-  );
-});
+  )
+})
 
 // the fetch event handler, to intercept requests and serve all 
 // static assets from the cache
 self.addEventListener('fetch', e => {
   e.respondWith(
     caches.match(e.request)
-    .then(response => response ? response : fetch(e.request))
+      .then(response => response || fetch(e.request))
   )
-});
+})
